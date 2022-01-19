1f01de9bf79a0523c3f7984dcdd7a07105decef1
JCIII <jcostello711@gmail.com>
2022-01-19 16:46:41 -0500

    app saves tasks to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,6 +64,7 @@ var createTaskEl = function(taskDataObj) {
 
     taskDataObj.id = taskIdCounter;
     tasks.push(taskDataObj);
+    saveTasks();
 
     //increase task counter for next unique id
     taskIdCounter++;
@@ -142,6 +143,7 @@ var updatedTaskArr = [];
 
     // reassign tasks array to be the same as updatedTaskArr
     tasks = updatedTaskArr;
+    saveTasks();
 };
 
 var editTask = function(taskId) {
@@ -174,6 +176,7 @@ var completeEditTask = function(taskName, taskType, taskId) {
         }
     };
 
+    saveTasks();
     formEl.removeAttribute("data-task-id");
     document.querySelector("#save-task").textContent = "Add Task";
 };
@@ -204,7 +207,13 @@ var taskStatusChangeHandler = function(event) {
         tasks[i].status = statusValue;
         }
     }
+
+    saveTasks();
 };
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
